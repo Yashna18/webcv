799e33bc84eadec0a2e675c9ba10fcdb9d224115
Yashna Shetty <yashna@Yashnas-MacBook-Pro.local>
2023-01-11 16:45:14 +0530

    navbar icon and link to homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
+        <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                
+                <Link to="/" className="navbar-logo">
+                    WEB CV <i className="fa-solid fa-thumbs-up fa-5x"></i>
+                </Link>
             </div>
         </nav>
+        </>
     );
 }
 
